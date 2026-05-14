0b09218c1bd879360dca94e743e20116fcdbeb40
Fatlind Azemi <fatlindazemi@gmail.com>
2026-05-14 05:38:07 +0200

    feat(page): assemble main page with all kinetic sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
+import Hero from './components/hero/Hero'
+import Expertise from './components/expertise/Expertise'
+import Projects from './components/projects/Projects'
+import Footer from './components/footer/Footer'
+import Navigation from './components/Navigation'
+import ParallaxBackground from './components/ParallaxBackground'
+
 export default function App() {
-  return null
+  return (
+    <>
+      <ParallaxBackground />
+      <Navigation />
+      <main>
+        <Hero />
+        <Expertise />
+        <Projects />
+        <Footer />
+      </main>
+    </>
+  )
 }
